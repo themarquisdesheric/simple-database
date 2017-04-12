2a25662da9e9699797d71ae76f9f3837b01241e2
Yuval Allweil <themarquisdesheric@gmail.com>
2017-04-12 15:56:47 -0700

    Testing suite works

--- test/db.test.js
+++ test/db.test.js
@@ -4,52 +4,73 @@ const dbFactory = require('../lib/db-factory');
 const TEST_DIR = './data';
 const db = dbFactory(TEST_DIR);
 
-describe('db.get()', () => {
+describe('db', () => {
+  describe('db.get()', () => {
 
-  it('gets a cat given an id', done => {
-    db.get('cats', 'f1de5', (err, data) => {
-      assert.deepEqual(data, {
-        'name': 'fluffy', 
-        '_id': 'f1de5'
+    it('gets a cat given an id', done => {
+      db.get('cats', 'f1de5', (err, data) => {
+        assert.deepEqual(data, {
+          'name': 'fluffy', 
+          '_id': 'f1de5'
+        });
+        done();
       });
-      done();
     });
-  });
 
-  it('gets a cat given an id', done => {
-    db.get('cats', '5khkh4', (err, data) => {
-      assert.deepEqual(data, {
-        'name': 'sparky', 
-        '_id': '5khkh4'
+    it('gets a cat given an id', done => {
+      db.get('cats', '5khkh4', (err, data) => {
+        assert.deepEqual(data, {
+          'name': 'sparky', 
+          '_id': '5khkh4'
+        });
+        done();
       });
-      done();
     });
-  });
 
-  it('gets a dog given an id', done => {
-    db.get('dogs', '39dkj7', (err, data) => {
-      assert.deepEqual(data, {
-        'name': 'pooch', 
-        '_id': '39dkj7'
+    it('gets a dog given an id', done => {
+      db.get('dogs', '39dkj7', (err, data) => {
+        assert.deepEqual(data, {
+          'name': 'pooch', 
+          '_id': '39dkj7'
+        });
+        done();
       });
-      done();
     });
-  });
 
-  it('gets a dog given an id', done => {
-    db.get('dogs', 'hk333', (err, data) => {
-      assert.deepEqual(data, {
-        'name': 'doggy', 
-        '_id': 'hk333'
+    it('gets a dog given an id', done => {
+      db.get('dogs', 'hk333', (err, data) => {
+        assert.deepEqual(data, {
+          'name': 'doggy', 
+          '_id': 'hk333'
+        });
+        done();
       });
-      done();
+    });
+
+    it('returns null when it can\'t find an object with that id', done => {
+      db.get('cats', 'doesnotexist', (err, data) => {
+        assert.equal(data, null);
+        done();
+      });
     });
   });
 
-  it('returns null when it can\'t find an object with that id', done => {
-    db.get('cats', 'doesnotexist', (err, data) => {
-      assert.equal(data, null);
-      done();
+  describe('db.save', () => {
+
+    it('saves the data in a file and returns that object with a generated id', done => {
+      const maru = {
+        name: 'maru',
+        type: 'scottish fold'
+      };
+
+      db.save('cats', maru, (err, cat) => {
+        if (err) return done(err);
+        assert.equal(cat.name, maru.name);
+        assert.ok(cat._id);
+        done();
+      });
     });
+
   });
+
 });
