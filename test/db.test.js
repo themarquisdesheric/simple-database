e61bc17394ee182a8665a2dbaa67574a2990a595
Yuval Allweil <themarquisdesheric@gmail.com>
2017-04-13 16:21:11 -0700

    Second test passing

--- test/db.test.js
+++ test/db.test.js
@@ -7,9 +7,10 @@ const db = dbFactory(TEST_DIR);
 
 const testCat1 = { name: 'testcat1', type: 'best'};
 const testCat2 = { name: 'testcat2', type: 'bestcat'};
-
+const testCat3 = { name: 'testcat2', type: 'bestcat'};
 const testDog1 = { name: 'testDog1', type: 'dawg'};
 const testDog2 = { name: 'testDog2', type: 'bestdawg'};
+let baobao = { name: 'baobao', type: 'panda'};
 
 describe('db', () => {
 
@@ -110,11 +111,6 @@ describe('db', () => {
     });
 
     it('creates a directory if none exists', done => {
-      const baobao = {
-        name: 'baobao',
-        type: 'panda'
-      };
-
       db.save('bears', baobao, (err, data) => {
         if (err) return done(err);
         db.get('bears', data._id, (err, bear) => {
@@ -151,6 +147,42 @@ describe('db', () => {
         done();
       });
     });
+
+    after(done => {
+      db.save('bears', baobao, (err, bear) => {
+        if (err) return done(err);
+        delete baobao._id;
+        done();
+      });
+    });
+  });
+
+  describe('db.update()', () => {
+
+    before(done => {
+      db.save('cats', testCat3, (err, cat) => {
+        if (err) return done(err);
+        testCat3._id = cat._id;
+        done();
+      });
+    });
+
+    it('should read the id property of the object if it is found', done => {
+      db.update('cats', testCat3, (err, cat) => {
+        if (err) return done(err);
+        assert.equal(cat, testCat3._id);
+        done();
+      });
+    });
+
+    it('should throw an error if object is not found', done => {
+      db.update('bears', baobao, (err, bear) => {
+        if (!err) return done(err);
+        assert.equal(err, 'Error: Expected object to have an _id property');
+        done();
+      });
+    });
+
   });
 
 });
